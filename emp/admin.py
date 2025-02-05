bfae61968c50a9c75a49fe3a0b0eeac80b2af2a6
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2025-02-05 13:59:34 +0530

    Third commit

--- emp/admin.py
+++ emp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import emp
+
+class EmpAdmin(admin.ModelAdmin):
+    list_display = ('name', 'working', 'emp_id')
+
+admin.site.register(emp)
